fd231dc9902f9d9fc61df8dfa5df94ac12a6a8af
Andrey Kovalenko <ak@taucode.com>
2022-07-29 02:18:28 +0300

    Updated content for 1.3.7

---
+++ dev-build-release.bat
@@ -0,0 +1,9 @@
+dotnet restore
+
+dotnet build --configuration Debug
+dotnet build --configuration Release
+
+dotnet test -c Debug .\test\TauCode.CopperLisp.Tests\TauCode.CopperLisp.Tests.csproj
+dotnet test -c Release .\test\TauCode.CopperLisp.Tests\TauCode.CopperLisp.Tests.csproj
+
+nuget pack nuget\TauCode.CopperLisp.nuspec
