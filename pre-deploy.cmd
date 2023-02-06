7479014937eb7f3ab2f665d2427a0c7c344637b9
Andrey Kovalenko <ak@taucode.com>
2023-02-06 18:15:07 +0200

    Winter resurrection - 2023.

--- pre-deploy.cmd
+++ pre-deploy.cmd
@@ -1,12 +1,9 @@
 dotnet restore
 
-dotnet clean --configuration Debug
-dotnet clean --configuration Release
+dotnet build TauCode.CopperLisp.sln -c Debug
+dotnet build TauCode.CopperLisp.sln -c Release
 
-dotnet build --configuration Debug
-dotnet build --configuration Release
-
-dotnet test -c Debug .\test\TauCode.CopperLisp.Tests\TauCode.CopperLisp.Tests.csproj
-dotnet test -c Release .\test\TauCode.CopperLisp.Tests\TauCode.CopperLisp.Tests.csproj
+dotnet test TauCode.CopperLisp.sln -c Debug
+dotnet test TauCode.CopperLisp.sln -c Release
 
 nuget pack nuget\TauCode.CopperLisp.nuspec
